b3ec5af99f83fbd3b9c364fea23bf3bea65969a6
diskree <diskreee@gmail.com>
2024-12-09 17:51:41 +0400

    update Stardust Labs datapacks

--- src/main/java/com/diskree/achievetodo/ExternalPack.java
+++ src/main/java/com/diskree/achievetodo/ExternalPack.java
@@ -59,27 +59,27 @@ public enum ExternalPack {
         "Terralith",
         Formatting.GREEN,
         "https://www.planetminecraft.com/data-pack/terralith-overworld-evolved-100-biomes-caves-and-more/",
-        "https://github.com/Stardust-Labs-MC/Terralith/releases/download/v2.4.11/Terralith_1.20.4_v2.4.11.zip",
+        "https://cdn.modrinth.com/data/8oi3bsk5/versions/PcYlKx8w/Terralith_1.21_v2.5.7.zip",
         null,
-        "c7e7f40e6a4da281bbd26eff16a3c7417b096b85",
+        "9645d4c557e8419154cc5775c5fe3ba9e82bfb8f",
         true
     ),
     AMPLIFIED_NETHER(
         "Amplified Nether",
         Formatting.DARK_RED,
         "https://www.planetminecraft.com/data-pack/amplified-nether-1-18/",
-        "https://github.com/Stardust-Labs-MC/Amplified-Nether/releases/download/v1.2.4/Amplified_Nether_1.20.4_v1.2.4.zip",
+        "https://cdn.modrinth.com/data/wXiGiyGX/versions/jfHNaJaE/Amplified_Nether_1.21_v1.2.7.zip",
         null,
-        "c67ce6110fe14271da660598384cffb44591c49b",
+        "473ddf1042ae7d4c19eff01944d0b9da0f11c831",
         true
     ),
     NULLSCAPE(
         "Nullscape",
         Formatting.DARK_PURPLE,
         "https://www.planetminecraft.com/data-pack/nullscape/",
-        "https://github.com/Stardust-Labs-MC/Nullscape/releases/download/v1.2.4/Nullscape_1.20.4_v1.2.4.zip",
+        "https://cdn.modrinth.com/data/LPjGiSO4/versions/J4B2BaWk/Nullscape_1.21_v1.2.10.zip",
         null,
-        "2554559b4ab7d498c64ba5157515cd3f16d77d6d",
+        "0a55bff36ab26b13963213ed1482d1b8b84ab568",
         true
     );
 
