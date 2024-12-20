31cd1e35edc24889aeddc7760b6d0edea55642de
diskree <diskreee@gmail.com>
2024-12-21 00:30:01 +0400

    fix water bucket ability

--- src/main/java/com/diskree/achievetodo/mixin/BucketItemMixin.java
+++ src/main/java/com/diskree/achievetodo/mixin/BucketItemMixin.java
@@ -3,17 +3,25 @@ package com.diskree.achievetodo.mixin;
 import com.diskree.achievetodo.AbilityType;
 import com.diskree.achievetodo.AchieveToDo;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.fluid.Fluid;
+import net.minecraft.fluid.Fluids;
 import net.minecraft.item.BucketItem;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(BucketItem.class)
 public class BucketItemMixin {
+
+    @Shadow
+    @Final
+    private Fluid fluid;
 
     @Inject(
         method = "placeFluid",
@@ -52,7 +60,7 @@ public class BucketItemMixin {
         BlockHitResult hitResult,
         CallbackInfoReturnable<Boolean> cir
     ) {
-        if (AchieveToDo.isAbilityLocked(player, AbilityType.USING_WATER_BUCKET)) {
+        if (fluid == Fluids.WATER && AchieveToDo.isAbilityLocked(player, AbilityType.USING_WATER_BUCKET)) {
             cir.setReturnValue(false);
         }
     }
