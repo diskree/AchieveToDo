6a860ba4d1bfb02e94bdbd65996b7f73522a2589
diskree <diskreee@gmail.com>
2024-12-26 23:22:33 +0400

    fix(abilities): don't show movement tutorial when vision ability is locked

---
+++ src/main/java/com/diskree/achievetodo/mixin/client/MovementTutorialStepHandlerMixin.java
@@ -0,0 +1,37 @@
+package com.diskree.achievetodo.mixin.client;
+
+import com.diskree.achievetodo.AbilityType;
+import com.diskree.achievetodo.AchieveToDo;
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.client.tutorial.MovementTutorialStepHandler;
+import net.minecraft.client.tutorial.TutorialManager;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(MovementTutorialStepHandler.class)
+public class MovementTutorialStepHandlerMixin {
+
+    @Shadow
+    @Final
+    private TutorialManager manager;
+
+    @Inject(
+        method = "tick",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/client/tutorial/TutorialManager;getClient()Lnet/minecraft/client/MinecraftClient;",
+            shift = At.Shift.BEFORE
+        ),
+        cancellable = true
+    )
+    public void scheduleUntilVisionAbilityUnlocked(CallbackInfo ci) {
+        ClientPlayerEntity player = manager.getClient().player;
+        if (player != null && AchieveToDo.isAbilityLocked(player, AbilityType.VISION, true)) {
+            ci.cancel();
+        }
+    }
+}
