9696d4b2e8a1a4bdeece8aa51c02b2562b094454
diskree <diskreee@gmail.com>
2024-12-30 23:22:06 +0400

    feat(gui): show obtained advancements count instead of XP on death screen in hardcore mode

---
+++ src/main/java/com/diskree/achievetodo/injection/mixin/client/DeathScreenMixin.java
@@ -0,0 +1,45 @@
+package com.diskree.achievetodo.injection.mixin.client;
+
+import com.diskree.achievetodo.client.AchieveToDoClient;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import net.minecraft.client.gui.screen.DeathScreen;
+import net.minecraft.text.MutableText;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
+import org.jetbrains.annotations.NotNull;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(DeathScreen.class)
+public class DeathScreenMixin {
+
+    @Shadow
+    @Final
+    private boolean isHardcore;
+
+    @WrapOperation(
+        method = "init",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/text/Text;translatable(Ljava/lang/String;[Ljava/lang/Object;)Lnet/minecraft/text/MutableText;"
+        )
+    )
+    private @NotNull MutableText setAdvancementsCountInsteadScoreInHardcore(
+        String key,
+        Object[] args,
+        @NotNull Operation<MutableText> original
+    ) {
+        if (isHardcore && !AchieveToDoClient.isNotReady()) {
+            return Text.translatable("key.advancements")
+                .append(": ")
+                .append(
+                    Text.of(String.valueOf(AchieveToDoClient.getObtainedAdvancementsCount())).copy()
+                        .formatted(Formatting.YELLOW)
+                );
+        }
+        return original.call(key, args);
+    }
+}
